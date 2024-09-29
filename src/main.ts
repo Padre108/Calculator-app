bd061a103ebadc6fb8f65adb8785e4810a0d85ca
Padre108 <nonoyjpgerona@gmail.com>
2024-09-29 23:52:58 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+
+
+Object calculator = {
+// Math operations
+const add: Function = (a: number, b: number): number => a + b;
+const subtract: Function = (a: number, b: number): number => a - b;
+const divide: Function = (a: number, b: number): number => a / b;
+const multiply: Function = (a: number, b: number): number => a * b;
+const power: Function = (a: number, b: number): number => Math.pow(a, b);
+
+// map symbols to math operations
+const operations: { [index: string]: Function } = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+  "^": power,
+};
+
+
+  function handleOndisplay() = {
+
+  }
+
+
+}
